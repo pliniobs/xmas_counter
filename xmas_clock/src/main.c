922528a5ce9e4e98ff2eb98511944ac8a07f4387
plinium <pliniobs@gmail.com>
2018-07-27 17:33:47 -0300

    Corrigido bug na SPI. Implementadas as funcionalidades do display matriz de LED's.

--- xmas_clock/src/main.c
+++ xmas_clock/src/main.c
@@ -22,18 +22,18 @@ int main(void)
 	__HAL_RCC_GPIOD_CLK_ENABLE();
 	__HAL_RCC_GPIOE_CLK_ENABLE();
 
-	/* create task */
-	Task_Create_Result = xTaskCreate(vTask_LED_Blink,						/* Pointer to the task entry function */
-			"vTask_LED_Blink",						/* A descriptive name for the task */
-			configMINIMAL_STACK_SIZE,				/* The number of words (not bytes!) to allocate for use as the task's stack */
-			(void *)NULL,							/* A value that will passed into the created task as the task's parameter */
-			1,										/* The priority at which the created task will execute */
-			NULL);									/* Used to pass a handle to the created task out of the xTaskCreate() function.*/
-
-	if( Task_Create_Result != pdPASS )
-	{
-		while(1); // Falha ao criar a tarefa
-	}
+//	/* create task */
+//	Task_Create_Result = xTaskCreate(vTask_LED_Blink,						/* Pointer to the task entry function */
+//			"vTask_LED_Blink",						/* A descriptive name for the task */
+//			configMINIMAL_STACK_SIZE,				/* The number of words (not bytes!) to allocate for use as the task's stack */
+//			(void *)NULL,							/* A value that will passed into the created task as the task's parameter */
+//			1,										/* The priority at which the created task will execute */
+//			NULL);									/* Used to pass a handle to the created task out of the xTaskCreate() function.*/
+//
+//	if( Task_Create_Result != pdPASS )
+//	{
+//		while(1); // Falha ao criar a tarefa
+//	}
 
 	/* create task */
 	Task_Create_Result = xTaskCreate(vTask_Matrix_Display,						/* Pointer to the task entry function */
@@ -58,17 +58,10 @@ int main(void)
 
 void vTask_LED_Blink(void *pvParameters)
 {
-	GPIO_InitTypeDef LED_Config;
 
-	LED_Config.Mode = GPIO_MODE_OUTPUT_PP;
-	LED_Config.Pull = GPIO_NOPULL;
-	LED_Config.Speed = GPIO_SPEED_HIGH;
-	LED_Config.Pin = GPIO_PIN_5;
-	HAL_GPIO_Init(GPIOA, &LED_Config);
 
 	while(1)
 	{
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 		vTaskDelay(25/ portTICK_PERIOD_MS);
 	}
 }
--- xmas_clock/src/matrix_display.h
+++ xmas_clock/src/matrix_display.h
@@ -10,10 +10,28 @@
 
 #include "stm32f4xx_hal.h"
 
+#define OP_NOOP   0
+#define OP_DIGIT0 1
+#define OP_DIGIT1 2
+#define OP_DIGIT2 3
+#define OP_DIGIT3 4
+#define OP_DIGIT4 5
+#define OP_DIGIT5 6
+#define OP_DIGIT6 7
+#define OP_DIGIT7 8
+#define OP_DECODEMODE  9
+#define OP_INTENSITY   10
+#define OP_SCANLIMIT   11
+#define OP_SHUTDOWN    12
+#define OP_DISPLAYTEST 15
+
+
 void vTask_Matrix_Display(void *pvParameters);
 
 void Matrix_Display_Init(void);
 
 void Matrix_Display_Write(uint8_t address, uint8_t data);
+
+void Cpy_Font_To_Buffer(uint8_t Caracter, uint8_t *Buffer, uint16_t Position);
 
 #endif /* MATRIX_DISPLAY_H_ */
